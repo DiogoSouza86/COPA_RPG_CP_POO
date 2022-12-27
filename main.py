78c1b98a8c561f98e8df7d5c3494911c9775fd77
Diogo Henrique Cruz Souza <100419674+DiogoSouza86@users.noreply.github.com>
2022-12-27 19:46:57 -0300

    chave gols com pontos4

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ class CopaDoMundo():
         self.selecoes_classificadas = []
         self.selecoes_fracos = []
         self.selecoes_medios = []
-        self.selecoes_fortes = []
+        self.selecoes_fortes = ['Nigth City']
         self.chave_a = []
         self.chave_b = []
         self.chave_c = []
@@ -19,7 +19,7 @@ class CopaDoMundo():
 
 
     def seleciona_32_selocoes(self):
-        cup = ["França", "Inglaterra", "Brasil"]
+        cup = ["França", "Inglaterra", "Brasil", "Nigth City"]
         w_cup = []
         #abre o txt que contém paises selecionados e os add a lista linha
         arquivo = open("paises.txt", encoding='utf-8', mode='r')
@@ -44,13 +44,17 @@ class CopaDoMundo():
 
     def mede_forca_selecoes(self):
         for selecao in self.selecoes_classificadas:
-            roll = randint(1,6)
-            if roll == 1:
-                self.selecoes_fracos.append(selecao)
-            elif roll == 6:
-                self.selecoes_fortes.append(selecao)
-            else:
-                self.selecoes_medios.append(selecao)
+            if selecao != "Nigth City":
+                roll = randint(1,6)
+                if roll == 1:
+                    self.selecoes_fracos.append(selecao)
+                elif roll == 6:
+                    self.selecoes_fortes.append(selecao)
+                else:
+                    self.selecoes_medios.append(selecao)
+
+    def set_ordena_selecoes_fortes(self):
+        return self.selecoes_fortes.sort()
 
     def separa_chaves2(self):
         sorteados_chave = []
@@ -108,28 +112,86 @@ class CopaDoMundo():
                     self.chave_h.append(item)
             chave.clear()
 
-
-    def partidas_chave(self):
-        lista_gols = gols = [0, 1, 2, 3, 4, 5]
+    def gols_a(self):
+        lista_gols =  [0, 1, 2, 3, 4, 5]
         if self.chave_a[0] in self.selecoes_fortes:
             if self.chave_a[1] in self.selecoes_fortes:
-                #lista_gols = [0, 1, 2, 3, 4, 5]
                 gols = choices(lista_gols, weights=[0.5, 20.5, 25, 25, 10, 10])
+                return gols
             elif self.chave_a[1] in self.selecoes_medios:
-                #lista_gols = [0, 1, 2, 3, 4, 5]
                 gols = choices(lista_gols, weights=[30, 30, 25, 0.5, 0.5, 0.5])
-            else:
-                #lista_gols = [0, 1, 2, 3, 4, 5]
+                return gols
+            elif self.chave_a[1] in self.selecoes_fracos:
                 gols = choices(lista_gols, weights=[0.5, 20.5, 25, 25, 10, 10])
+                return gols
         elif self.chave_a[0] in self.selecoes_medios:
             if self.chave_a[1] in self.selecoes_fortes:
                 gols = choices(lista_gols, weights=[30, 30, 25, 0.5, 0.5, 0.5])
-        else:
-            return 'fraco'
+                return gols
+            elif self.chave_a[1] in self.selecoes_medios:
+                gols = choices(lista_gols, weights=[30, 30, 25, 0.5, 0.5, 0.5])
+                return gols
+            elif self.chave_a[1] in self.selecoes_fracos:
+                gols = choices(lista_gols, weights=[10, 30, 30, 10, 10, 10])
+                return gols
+        elif self.chave_a[0] in self.selecoes_fracos:
+            if self.chave_a[1] in self.selecoes_fortes:
+                gols = choices(lista_gols, weights=[40.5, 30, 10, 0.5, 0.5, 0.5])
+                return gols
+            elif self.chave_a[1] in self.selecoes_medios:
+                gols = choices(lista_gols, weights=[30, 30, 10, 10, 10, 10])
+                return gols
+            elif self.chave_a[1] in self.selecoes_fracos:
+                gols = choices(lista_gols, weights=[30, 30, 20, 10, 0.5, 0.5])
+                return gols
 
-        return f'{self.chave_a[0]} {gols} X {self.chave_a[1]}\n' \
-               f'{self.chave_a[2]} X {self.chave_a[3]}\n'
+    def pontos4(self,time1, time2):
+            if time1 in (times_fortes) and time2 in (times_fracos):
+                gols = [0, 1, 2, 3, 4, 5]
+                ff_gols = choices(gols, weights=[0.5, 20.5, 25, 25, 10, 10])
+                return ff_gols
+            if time1 in (times_fortes) and time2 in (times_medios):
+                gols = [0, 1, 2, 3, 4, 5]
+                ff_gols = choices(gols, weights=[30, 30, 25, 0.5, 0.5, 0.5])
+                return ff_gols
+            if time1 in (times_fortes) and time2 in (times_fortes):
+                gols = [0, 1, 2, 3, 4, 5]
+                ff_gols = choices(gols, weights=[30, 30, 25, 0.5, 0.5, 0.5])
+                return ff_gols
 
+            if time1 in (times_medios) and time2 in (times_fortes):
+                gols = [0, 1, 2, 3, 4, 5]
+                ff_gols = choices(gols, weights=[30, 30, 25, 0.5, 0.5, 0.5])
+                return ff_gols
+            if time1 in (times_medios) and time2 in (times_medios):
+                gols = [0, 1, 2, 3, 4, 5]
+                ff_gols = choices(gols, weights=[30, 30, 25, 0.5, 0.5, 0.5])
+                return ff_gols
+            if time1 in (times_medios) and time2 in (times_fracos):
+                gols = [0, 1, 2, 3, 4, 5]
+                ff_gols = choices(gols, weights=[10, 30, 30, 10, 10, 10])
+                return ff_gols
+
+            if time1 in (times_fracos) and time2 in (times_fortes):
+                gols = [0, 1, 2, 3, 4, 5]
+                ff_gols = choices(gols, weights=[40.5, 30, 10, 0.5, 0.5, 0.5])
+                return ff_gols
+            elif time1 in (times_fracos) and time2 in (times_medios):
+                gols = [0, 1, 2, 3, 4, 5]
+                ff_gols = choices(gols, weights=[30, 30, 10, 10, 10, 10])
+                return ff_gols
+            elif time1 in (times_fracos) and time2 in (times_fracos):
+                gols = [0, 1, 2, 3, 4, 5]
+                ff_gols = choices(gols, weights=[30, 30, 20, 10, 0.5, 0.5])
+                return ff_gols
+
+    def pontos5(self, time1, time2):
+        if time1 in self.selecoes_fortes and time2 in self.selecoes_fortes:
+
+    def resultado_chave(self, g1, g2, g3, g4):
+
+        print( f'{self.chave_a[0]} {g1} X {g2} {self.chave_a[1]}\n'
+               f'{self.chave_a[2]} {g3} X {g4} {self.chave_a[3]}\n')
 
     def __str__(self):
         return f'COPA DO MUNDO NIGHT CITY\n' \
@@ -141,17 +203,15 @@ class CopaDoMundo():
                f'' \
 
 
-
-
-
-
-
-
-
 copa = CopaDoMundo()
 copa.seleciona_32_selocoes()
 copa.set_ordena_32_selecoes()
 copa.mede_forca_selecoes()
+copa.set_ordena_selecoes_fortes()
 copa.separa_chaves()
 print(copa)
-print(copa.partidas_chave())
+g1 = copa.gols_a()
+g2 = copa.gols_a()
+g3 = copa.gols_a()
+g4 = copa.gols_a()
+copa.resultado_chave(g1, g2, g3, g4)
